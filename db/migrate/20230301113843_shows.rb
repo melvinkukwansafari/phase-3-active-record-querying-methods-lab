2b8fef04672b5fcb6beed7e47764510177c1b615
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2023-03-01 15:09:20 +0300

    complete assignment

---
+++ db/migrate/20230301113843_shows.rb
@@ -0,0 +1,11 @@
+class Shows < ActiveRecord::Migration[6.1]
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+      t.timestamps
+    end
+  end
+end
